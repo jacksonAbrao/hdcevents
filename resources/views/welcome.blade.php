16d1283c94f5f905b24e7dbd81009ac1cc97fd18
jackson.comp1681@hotmail.com <jackson.comp1681@hotmail.com>
2022-02-16 15:00:09 -0300

    continuando o curso dia 3

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -13,15 +13,15 @@
 <div id="events-container" class="col-md-12">
     <h2>Próximos Eventos</h2>
     <p class="subititle">Veja os eventos dos próximos dias</p>
-    <div id="cards-container" class="row">
+    <div id="cards-container" class="row d-flex">
         @foreach($events as $event)
         <div class="card col-md-3">
-            <img src="/img/event_placeholder.jpg" alt="{{ $event->title }}">
+            <img src="/img/events/{{ $event->image }}" alt="{{ $event->title }}">
             <div class="card-body">
                 <p class="card-date">10/02/2022</p>
                 <h5 class="card-title">{{ $event->title }}</h5>
                 <p class="card-participants">X Participantes</p>
-                <a href="" class="btn btn-primary">Saber mais</a>
+                <a href="/events/{{ $event->id }}" class="btn btn-primary">Saber mais</a>
             </div>
         </div>
         @endforeach
